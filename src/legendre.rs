fd74aec057b700a1658d2d4f553192b14da5951a
jorenham <jhammudoglu@gmail.com>
2025-09-06 23:27:47 +0200

    remove assoc legendre `norn` param, and add smoke tests

--- src/legendre.rs
+++ src/legendre.rs
@@ -62,11 +62,99 @@ pub fn sph_legendre_p<T: LegendreArg>(n: i32, m: i32, z: T) -> T::Output {
 }
 
 /// Associated Legendre polynomial of the first kind
-pub fn assoc_legendre_p<T: LegendreArg>(n: i32, m: i32, z: T, branch_cut: u8) -> T::Output {
-    z.assoc_legendre_p(n as c_int, m as c_int, branch_cut as c_int)
+pub fn assoc_legendre_p<T: LegendreArg>(n: i32, m: i32, z: T) -> T::Output {
+    z.assoc_legendre_p(n as c_int, m as c_int, 2)
 }
 
 /// Normalized associated Legendre polynomial of the first kind
-pub fn assoc_legendre_p_norm<T: LegendreArg>(n: i32, m: i32, z: T, branch_cut: u8) -> T::Output {
-    z.assoc_legendre_p_norm(n as c_int, m as c_int, branch_cut as c_int)
+pub fn assoc_legendre_p_norm<T: LegendreArg>(n: i32, m: i32, z: T) -> T::Output {
+    z.assoc_legendre_p_norm(n as c_int, m as c_int, 2)
+}
+
+#[cfg(test)]
+mod tests {
+    // there are no xsref tables, so we use smoke tests instead
+
+    use super::*;
+    use num_complex::c64;
+    use std::f64::consts;
+
+    const I: Complex<f64> = Complex::new(0.0, 1.0);
+
+    // legendre_p
+
+    #[test]
+    fn test_legendre_p_f64() {
+        assert_eq!(legendre_p(0, 0.0), 1.0);
+        assert_eq!(legendre_p(1, 0.0), 0.0);
+        assert_eq!(legendre_p(2, 0.0), -0.5);
+    }
+
+    #[test]
+    fn test_legendre_p_c64() {
+        assert_eq!(legendre_p(0, I), c64(1.0, 0.0));
+        assert_eq!(legendre_p(1, I), I);
+        assert_eq!(legendre_p(2, I), c64(-2.0, 0.0));
+    }
+
+    // sph_legendre_p
+
+    #[test]
+    fn test_sph_legendre_p_f64() {
+        assert_eq!(sph_legendre_p(0, 0, 0.0), 0.28209479177387814);
+        assert_eq!(sph_legendre_p(1, 0, 0.0), 0.48860251190291987);
+        assert_eq!(sph_legendre_p(1, 1, 0.0), 0.0);
+    }
+
+    #[test]
+    fn test_sph_legendre_p_c64() {
+        assert_eq!(sph_legendre_p(0, 0, I), c64(0.2820947917738782, 0.0));
+        assert_eq!(sph_legendre_p(1, 0, I), c64(0.7539530742394804, 0.0));
+        assert_eq!(sph_legendre_p(1, 1, I), c64(-0.4060251368556634, 0.0));
+    }
+
+    // assoc_legendre_p
+
+    #[test]
+    fn test_assoc_legendre_p_f64() {
+        assert_eq!(assoc_legendre_p(0, 0, 0.0), 1.0);
+        assert_eq!(assoc_legendre_p(1, 0, 0.0), 0.0);
+        assert_eq!(assoc_legendre_p(0, 1, 0.0), 0.0);
+        assert_eq!(assoc_legendre_p(1, 1, 0.0), -1.0);
+    }
+
+    #[test]
+    fn test_assoc_legendre_p_c64() {
+        assert_eq!(assoc_legendre_p(0, 0, I), c64(1.0, 0.0));
+        assert_eq!(assoc_legendre_p(1, 0, I), I);
+        assert_eq!(assoc_legendre_p(0, 1, I), c64(0.0, 0.0));
+        assert_eq!(assoc_legendre_p(1, 1, I), c64(-consts::SQRT_2, 0.0));
+    }
+
+    // assoc_legendre_p_norm
+
+    #[test]
+    fn test_assoc_legendre_p_norm_f64() {
+        assert!((assoc_legendre_p_norm(0, 0, 0.0) - consts::FRAC_1_SQRT_2).abs() < f64::EPSILON);
+        assert_eq!(assoc_legendre_p_norm(1, 0, 0.0), 0.0);
+        assert_eq!(assoc_legendre_p_norm(0, 1, 0.0), 0.0);
+        assert_eq!(
+            assoc_legendre_p_norm(1, 1, 0.0),
+            -0.8660254037844386 // -sqrt(3 / 4)
+        );
+    }
+
+    #[test]
+    fn test_assoc_legendre_p_norm_c64() {
+        assert!((assoc_legendre_p_norm(0, 0, I).re - consts::FRAC_1_SQRT_2).abs() < f64::EPSILON);
+        assert_eq!(
+            assoc_legendre_p_norm(1, 0, I),
+            c64(0.0, 1.224744871391589) // sqrt(3 / 2) i
+        );
+        assert_eq!(assoc_legendre_p_norm(0, 1, I), c64(0.0, 0.0));
+        assert_eq!(
+            assoc_legendre_p_norm(1, 1, I),
+            c64(-1.2247448713915892, 0.0) // -sqrt(3 / 2)
+        );
+    }
 }
