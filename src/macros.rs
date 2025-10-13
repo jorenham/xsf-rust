84f3b131c4a35b25e1847ac97c8c3783ac6dae0c
jorenham <jhammudoglu@gmail.com>
2025-10-13 05:49:44 +0200

    context-dependent `np_assert_allclose` tolerance defaults

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,31 @@
 /// Similar to `numpy.testing.assert_allclose`
 #[macro_export]
 macro_rules! np_assert_allclose {
-    ($actual:expr, $expected:expr $(, rtol = $rtol:expr)? $(, atol = $atol:expr)?) => {{
+    // Both rtol and atol provided
+    ($actual:expr, $expected:expr, rtol = $rtol:expr, atol = $atol:expr) => {{
+        $crate::np_assert_allclose!(@impl $actual, $expected, $rtol, $atol)
+    }};
+    ($actual:expr, $expected:expr, atol = $atol:expr, rtol = $rtol:expr) => {{
+        $crate::np_assert_allclose!(@impl $actual, $expected, $rtol, $atol)
+    }};
+    // Only rtol provided - atol defaults to 0.0
+    ($actual:expr, $expected:expr, rtol = $rtol:expr) => {{
+        $crate::np_assert_allclose!(@impl $actual, $expected, $rtol, 0.0)
+    }};
+    // Only atol provided - rtol defaults to 0.0
+    ($actual:expr, $expected:expr, atol = $atol:expr) => {{
+        $crate::np_assert_allclose!(@impl $actual, $expected, 0.0, $atol)
+    }};
+    // Neither provided - rtol = 1e-7, atol = 0.0
+    ($actual:expr, $expected:expr) => {{
+        $crate::np_assert_allclose!(@impl $actual, $expected, 1e-7, 0.0)
+    }};
+    // Internal implementation
+    (@impl $actual:expr, $expected:expr, $rtol:expr, $atol:expr) => {{
         let actual = $actual;
         let expected = $expected;
-        let rtol = $crate::np_assert_allclose!(@default_rtol $($rtol)?);
-        let atol = $crate::np_assert_allclose!(@default_atol $($atol)?);
+        let rtol: f64 = $rtol;
+        let atol: f64 = $atol;
 
         assert_eq!(actual.len(), expected.len());
         for (&a, &e) in actual.iter().zip(expected.iter()) {
@@ -28,10 +48,6 @@ macro_rules! np_assert_allclose {
             }
         }
     }};
-    (@default_rtol) => { 1e-7 };
-    (@default_rtol $rtol:expr) => { $rtol };
-    (@default_atol) => { 0.0 };
-    (@default_atol $atol:expr) => { $atol };
 }
 
 /// Similar to `numpy.testing.assert_equal`
