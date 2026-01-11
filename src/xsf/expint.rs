6d889b4308bff65332c12958138542ec7b5a1475
jorenham <jhammudoglu@gmail.com>
2026-01-11 11:30:41 +0100

    improved exponential integral function docs

--- src/xsf/expint.rs
+++ src/xsf/expint.rs
@@ -33,61 +33,75 @@ impl ExpIntArg for num_complex::Complex<f64> {
     }
 }
 
-/// Exponential integral E₁ for real or complex input
+/// Exponential integral $E_1$ for real or complex input
 ///
-/// For complex z ≠ 0 the exponential integral can be defined as
+/// Corresponds to [`scipy.special.exp1`][exp1]
 ///
-/// E₁(z) = ∫[z,∞) e⁻ᵗ / t dt,
+/// [exp1]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.exp1.html
+///
+/// # Definition
+/// For complex $z \neq 0$ the exponential integral can be defined as
+///
+/// $$ E_1(z) = \int_z^\infty \frac{e^{-t}}{t} \dd t $$
 ///
 /// where the path of the integral does not cross the negative real axis or pass through the origin.
 ///
-/// ## See also:
-/// - [`expi`]: exponential integral Ei
-/// - [`expn`](fn.expn.html): generalization of E₁
+/// # See also
+/// - [`scaled_exp1`]: scaled version of $E_1$
+/// - [`expn`](crate::expn): generalization of $E_1$
+/// - [`expi`]: exponential integral $Ei$
 #[must_use]
 #[inline]
 pub fn exp1<T: ExpIntArg>(z: T) -> T {
     z.exp1()
 }
 
-/// Exponential integral Ei for real or complex input
+/// Exponential integral $Ei$ for real or complex input
 ///
-/// For real x, the exponential integral is defined as
+/// # Definition
+/// For real $x$, the exponential integral is defined as
 ///
-/// Ei(x) = ∫(−∞,x] eᵗ / t dt.
+/// $$ Ei(x) = \int_{-\infty}^x \frac{e^t}{t} \dd t $$
 ///
 /// For x > 0 the integral is understood as a Cauchy principal value.
 ///
 /// It is extended to the complex plane by analytic continuation of the function on the interval
-/// (0, ∞). The complex variant has a branch cut on the negative real axis.
+/// $(0, \infty)$. The complex variant has a branch cut on the negative real axis.
 ///
-/// ## See also:
-/// - [`exp1`]: exponential integral E₁
-/// - [`expn`](fn.expn.html): generalization of E₁
+/// # See also
+/// - [`exp1`]: exponential integral $E_1$
+/// - [`expn`](crate::expn): generalization of $E_1$
 #[must_use]
 #[inline]
 pub fn expi<T: ExpIntArg>(z: T) -> T {
     z.expi()
 }
 
-/// Scaled version of the exponential integral E₁ for real input
+/// Scaled version of the exponential integral $E_1$ for real input
 ///
-/// This function computes F(x), where F is the factor remaining in E₁(x)
-/// when e⁻ˣ/x is factored out. That is:
+/// There exists no corresponding function in `scipy.special`.
 ///
-/// E₁(x) = x⁻¹ e⁻ˣ F(x)
+/// # Definition
+/// This function computes $F(x)$, where $F$ is the factor remaining in $E_1(x)$
+/// when $e^{-x}/x$ is factored out. That is:
+///
+/// $$ E_1(x) = x^{-1} e^{-x} F(x) $$
 ///
 /// or
 ///
-/// F(x) = x eˣ E₁(x)
+/// $$ F(x) = x e^{x} E_1(x) $$
 ///
-/// The function is defined for real x ≥ 0. For x < 0, NaN is returned.
+/// The function is defined for real $x \geq 0$. For $x < 0$, NaN is returned.
 ///
-/// F has the properties:
+/// $F$ has the properties:
 ///
-/// - F(0) = 0
-/// - F(x) is increasing on [0, ∞).
-/// - F(x) = 1 in the limit as x → ∞.
+/// - $F(0) = 0$
+/// - $F(x)$ is increasing on $[0, \infty)$.
+/// - $F(x) = 1$ in the limit as $x \to \infty$.
+///
+/// # See also
+/// - [`exp1`]: exponential integral $E_1$
+/// - [`expi`]: exponential integral $Ei$
 #[must_use]
 #[inline]
 pub fn scaled_exp1(x: f64) -> f64 {
