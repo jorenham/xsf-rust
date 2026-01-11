772bffc503314fd796f86e4122b6c6ed3ec5668e
jorenham <jhammudoglu@gmail.com>
2026-01-11 12:41:33 +0100

    improved trig function docs

--- src/xsf/trig.rs
+++ src/xsf/trig.rs
@@ -33,56 +33,116 @@ impl TrigArg for num_complex::Complex<f64> {
     }
 }
 
-/// Compute `sin(pi*z)` for real or complex `z`
+/// $sin(\pi z)$ for real or complex $z$
+///
+/// Has no corresponding function in `scipy.special`.
+///
+/// # See also
+/// - [`cospi`]
 #[must_use]
 #[inline]
 pub fn sinpi<T: TrigArg>(z: T) -> T {
     z.sinpi()
 }
 
-/// Compute `cos(pi*z)` for real or complex `z`
+/// $cos(\pi z)$ for real or complex $z$
+///
+/// Has no corresponding function in `scipy.special`.
+///
+/// # See also
+/// - [`sinpi`]
 #[must_use]
 #[inline]
 pub fn cospi<T: TrigArg>(z: T) -> T {
     z.cospi()
 }
 
-/// Circular sine of angle in degrees
+/// Sine of the angle x given in degrees.
+///
+/// Corresponds to [`scipy.special.sindg`][scipy].
+///
+/// [scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.sindg.html
+///
+/// # See also
+/// - [`cosdg`]
+/// - [`tandg`]
+/// - [`cotdg`]
 #[must_use]
 #[inline]
 pub fn sindg(x: f64) -> f64 {
     unsafe { crate::ffi::xsf::sindg(x) }
 }
 
-/// Circular cosine of angle in degrees
+/// Cosine of angle in degrees
+///
+/// Corresponds to [`scipy.special.cosdg`][scipy].
+///
+/// [scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.cosdg.html
+///
+/// # See also
+/// - [`sindg`]
+/// - [`tandg`]
+/// - [`cotdg`]
 #[must_use]
 #[inline]
 pub fn cosdg(x: f64) -> f64 {
     unsafe { crate::ffi::xsf::cosdg(x) }
 }
 
-/// Circular tangent of argument in degrees
+/// Tangent of angle x given in degrees
+///
+/// Corresponds to [`scipy.special.tandg`][scipy].
+///
+/// [scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.tandg.html
+///
+/// # See also
+/// - [`sindg`]
+/// - [`cosdg`]
+/// - [`cotdg`]
 #[must_use]
 #[inline]
 pub fn tandg(x: f64) -> f64 {
     unsafe { crate::ffi::xsf::tandg(x) }
 }
 
-/// Circular cotangent of argument in degrees
+/// Cotangent of the angle x given in degrees
+///
+/// Corresponds to [`scipy.special.cotdg`][scipy].
+///
+/// [scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.cotdg.html
+///
+/// # See also
+/// - [`sindg`]
+/// - [`cosdg`]
+/// - [`tandg`]
 #[must_use]
 #[inline]
 pub fn cotdg(x: f64) -> f64 {
     unsafe { crate::ffi::xsf::cotdg(x) }
 }
 
-/// Compute `cos(x) - 1`
+/// $\cos(x) - 1$ for use when $x$ is near zero
+///
+/// Corresponds to [`scipy.special.cosm1`][scipy].
+///
+/// [scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.cosm1.html
+///
+/// # See also
+/// - [`expm1`](crate::expm1)
+/// - [`log1p`](crate::log1p)
 #[must_use]
 #[inline]
 pub fn cosm1(x: f64) -> f64 {
     unsafe { crate::ffi::xsf::cosm1(x) }
 }
 
-/// Degrees, minutes, seconds to radians
+/// Convert from degrees to radians.
+///
+/// Returns the angle given in (d)egrees, (m)inutes, and (s)econds in radians.
+///
+/// Corresponds to [`scipy.special.radian`][scipy].
+///
+/// [scipy]: https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.radian.html
 #[must_use]
 #[inline]
 pub fn radian(d: f64, m: f64, s: f64) -> f64 {
